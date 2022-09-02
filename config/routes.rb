b0344cdb4b486a5596951e5029a83fe505738553
tylergubser <tylergubser2200@gmail.com>
2022-09-01 20:50:41 -0500

    login route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :users
 
   get '/', to: 'sessions#new', as: 'root'
+
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   
 end
