083fe333dac5d90275145edb7de2ba131efb52ae
tylergubser <tylergubser2200@gmail.com>
2022-09-01 20:48:49 -0500

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+
+    def create
+        
+        @user = User.find_by(email_address: params[:email])
+        
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            
+        else
+            redirect_to "/"
+        end
+    end
 end
